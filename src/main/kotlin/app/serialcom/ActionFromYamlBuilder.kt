83de722ce6948a67c7a270472b8c1393b7f7349f
AdamTMalek <malekadas@gmail.com>
2020-01-22 21:35:43 +0000

    Add comments to the action builder

--- src/main/kotlin/app/serialcom/ActionFromYamlBuilder.kt
+++ src/main/kotlin/app/serialcom/ActionFromYamlBuilder.kt
@@ -1,10 +1,20 @@
 package app.serialcom
 
+/**
+ * The [ActionFromYamlBuilder] takes a line coming from the serial port and builds an [Action] object by parsing
+ * the line.
+ * It exposes three public methods:
+ * - [set] that automatically sets the right fields of the [Action] object
+ * - [isReady] that indicates if the [Action] object is ready to be built or not
+ * - [build] that builds [Action] and returns it
+ */
 class ActionFromYamlBuilder {
     var action: String? = null
     var value: String? = null
 
-
+    /**
+     * Given the [line] set the correct field of the [Action] object
+     */
     fun set(line: String) {
         val (key, value) = parseLineAsPair(line)
 
@@ -14,10 +24,17 @@ class ActionFromYamlBuilder {
         }
     }
 
+    /**
+     * Returns true if the object is ready to be built
+     */
     fun isReady(): Boolean {
         return action != null && value != null
     }
 
+    /**
+     * Returns the [Action] object with the set fields
+     * @throws IllegalStateException when some fields were not set
+     */
     @Throws(IllegalStateException::class)
     fun build(): Action {
         if (!isReady())
@@ -27,6 +44,8 @@ class ActionFromYamlBuilder {
     }
 
     private fun parseLineAsPair(line: String): Pair<String, String> {
+        // First group is a match of a word or bunch of words to a colon,
+        // the second is any alphanumerical character, dot or a colon.
         val regex = Regex("(\\w+): ([A-Za-z0-9:.]+)")
         val matchGroups = regex.find(line)!!.groups
 

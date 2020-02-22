7d167f0cb87cb184a9f4d2d9a22cc34f40819355
AdamTMalek <malekadas@gmail.com>
2020-02-22 21:47:09 +0000

    Create maintenance view

---
+++ src/main/kotlin/view/MaintenanceController.kt
@@ -0,0 +1,11 @@
+package view
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class MaintenanceController : Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+
+    }
+}

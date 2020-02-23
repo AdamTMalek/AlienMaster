d324ded19a532bf6af6b24de5bcfa7b81e30513c
AdamTMalek <malekadas@gmail.com>
2020-02-23 18:30:21 +0000

    Toggle LED on click

--- src/main/kotlin/view/MaintenanceController.kt
+++ src/main/kotlin/view/MaintenanceController.kt
@@ -1,17 +1,17 @@
 package view
 
 import app.OnMessageReceivedListener
-import app.serialcom.Action
-import app.serialcom.OnAvailablePortsChangeListener
-import app.serialcom.Serial
-import app.serialcom.StateMessage
+import app.serialcom.*
 import javafx.application.Platform
+import javafx.css.Styleable
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.ChoiceBox
+import javafx.scene.control.TextArea
+import javafx.scene.shape.Circle
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
@@ -19,6 +19,22 @@ import java.util.*
 class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvailablePortsChangeListener {
     @FXML
     private var portChoice = ChoiceBox<String>()
+    @FXML
+    private var led0 = Circle()
+    @FXML
+    private var led1 = Circle()
+    @FXML
+    private var led2 = Circle()
+    @FXML
+    private var led3 = Circle()
+    @FXML
+    private var led4 = Circle()
+    @FXML
+    private var led5 = Circle()
+    @FXML
+    private var logText = TextArea()
+
+    private val ledActiveStyle = "led-active"
 
     private lateinit var serial: Serial
 
@@ -79,4 +95,70 @@ class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvaila
     }
 
     private fun getAllAvailablePortsNames() = serial.getAllAvailablePorts().map { it.descriptivePortName }
+
+    fun onLed0Clicked() {
+        sendSetLedAction(0, isLedActive(led0))
+        toggleLedActive(led0)
+    }
+
+    fun onLed1Clicked() {
+        sendSetLedAction(1, isLedActive(led1))
+        toggleLedActive(led1)
+    }
+
+    fun onLed2Clicked() {
+        sendSetLedAction(2, isLedActive(led2))
+        toggleLedActive(led2)
+    }
+
+    fun onLed3Clicked() {
+        sendSetLedAction(3, isLedActive(led3))
+        toggleLedActive(led3)
+    }
+
+    fun onLed4Clicked() {
+        sendSetLedAction(4, isLedActive(led4))
+        toggleLedActive(led4)
+    }
+
+    fun onLed5Clicked() {
+        sendSetLedAction(5, isLedActive(led5))
+        toggleLedActive(led5)
+    }
+
+    private fun isLedActive(led: Styleable): Boolean {
+        return led.styleClass.contains(ledActiveStyle)
+    }
+
+    private fun toggleLedActive(led: Styleable) {
+        if (isLedActive(led))
+            led.styleClass.remove(ledActiveStyle)
+        else
+            led.styleClass.add(ledActiveStyle)
+    }
+
+    private fun sendSetLedAction(id: Int, on: Boolean) {
+        val value = if (on) 1 else 0
+        val action = Action(ActionType.SET, DeviceType.LED, id, value)
+        sendAction(action)
+    }
+
+    private fun sendAction(action: Action) {
+        val data = action.toYaml()
+        //serial.send(data) TODO: Uncomment after finished implementing the controller
+        addToLogTextView(data, true)
+    }
+
+    /**
+     * Adds the given [text] to the serial communication log view,
+     * if [sent] is set to true, the text will be prepended with >
+     * to indicate the direction of the message, otherwise it will
+     * be prepended with <
+     */
+    private fun addToLogTextView(text: String, sent: Boolean) {
+        val directionChar = if (sent) '>' else '<'
+        val indentedText = text.prependIndent("\t")
+
+        logText.text += directionChar + indentedText + "\n\n"
+    }
 }

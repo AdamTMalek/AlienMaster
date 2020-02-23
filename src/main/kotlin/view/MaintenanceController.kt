1d77c5dd07f3ba62146982ddebc6db7670cc4d70
AdamTMalek <malekadas@gmail.com>
2020-02-23 18:03:31 +0000

    Display available ports in the maintenance view

--- src/main/kotlin/view/MaintenanceController.kt
+++ src/main/kotlin/view/MaintenanceController.kt
@@ -2,21 +2,34 @@ package view
 
 import app.OnMessageReceivedListener
 import app.serialcom.Action
+import app.serialcom.OnAvailablePortsChangeListener
+import app.serialcom.Serial
 import app.serialcom.StateMessage
+import javafx.application.Platform
+import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.control.ChoiceBox
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
-class MaintenanceController : Initializable, OnMessageReceivedListener {
+class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvailablePortsChangeListener {
+    @FXML
+    private var portChoice = ChoiceBox<String>()
+
+    private lateinit var serial: Serial
+
     companion object {
-        fun showAndWait() {
+        fun showAndWait(serial: Serial) {
             val url = this::class.java.classLoader.getResource("view/maintenance_view.fxml")
             val loader = FXMLLoader().apply { location = url }
             val root = loader.load<Parent>()
+
+            val controller = loader.getController<MaintenanceController>()
+            controller.setSerial(serial)
 
             val scene = Scene(root, 900.0, 700.0)
             Stage().apply {
@@ -31,6 +44,24 @@ class MaintenanceController : Initializable, OnMessageReceivedListener {
 
     }
 
+    fun setSerial(serial: Serial) {
+        this.serial = serial
+        this.serial.addPortListener(this)
+        setupPortChoice()
+    }
+
+    private fun setupPortChoice() {
+        val serialNames = getAllAvailablePortsNames()
+
+        portChoice.items.addAll(serialNames)
+
+        val current = serial.getActivePort()
+            ?: return
+
+        val currentPortIndex = portChoice.items.indexOf(current.descriptivePortName)
+        portChoice.selectionModel.select(currentPortIndex)
+    }
+
     override fun onActionReceived(action: Action) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -38,4 +69,14 @@ class MaintenanceController : Initializable, OnMessageReceivedListener {
     override fun onStateReceived(state: StateMessage) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun onAvailablePortsChange() {
+        Platform.runLater {
+            portChoice.items.clear()
+            portChoice.items.addAll(getAllAvailablePortsNames())
+        }
+        println("Ports updated")
+    }
+
+    private fun getAllAvailablePortsNames() = serial.getAllAvailablePorts().map { it.descriptivePortName }
 }

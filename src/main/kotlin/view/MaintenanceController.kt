52956ad31c50b442f34cb6a30139ad57e991dd69
AdamTMalek <malekadas@gmail.com>
2020-02-23 19:20:29 +0000

    Handle buttons and card reader

--- src/main/kotlin/view/MaintenanceController.kt
+++ src/main/kotlin/view/MaintenanceController.kt
@@ -1,5 +1,6 @@
 package view
 
+import app.MessageParser
 import app.OnMessageReceivedListener
 import app.serialcom.*
 import javafx.application.Platform
@@ -10,33 +11,53 @@ import javafx.fxml.Initializable
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.ChoiceBox
+import javafx.scene.control.Label
 import javafx.scene.control.TextArea
 import javafx.scene.shape.Circle
+import javafx.scene.shape.Rectangle
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
-class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvailablePortsChangeListener {
+class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvailablePortsChangeListener,
+    OnSerialDataReceivedListener {
+
     @FXML
     private var portChoice = ChoiceBox<String>()
     @FXML
+    private var button0 = Rectangle()
+    @FXML
     private var led0 = Circle()
+    @FXML
+    private var button1 = Rectangle()
     @FXML
     private var led1 = Circle()
     @FXML
+    private var button2 = Rectangle()
+    @FXML
     private var led2 = Circle()
+    @FXML
+    private var button3 = Rectangle()
     @FXML
     private var led3 = Circle()
     @FXML
+    private var button4 = Rectangle()
+    @FXML
     private var led4 = Circle()
+    @FXML
+    private var button5 = Rectangle()
     @FXML
     private var led5 = Circle()
     @FXML
+    private var userIdLabel = Label()
+    @FXML
     private var logText = TextArea()
 
+    private val buttonPressedStyle = "button-pressed"
     private val ledActiveStyle = "led-active"
     private val goodAlienId = 0
     private val badAlienId = 1
+    private val messageParser = MessageParser()
 
     private lateinit var serial: Serial
 
@@ -59,6 +80,7 @@ class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvaila
     }
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+        messageParser.addListener(this)
         addAutoScrollToLogText()
     }
 
@@ -85,11 +107,45 @@ class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvaila
     }
 
     override fun onActionReceived(action: Action) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        addToLogTextView(action.toYaml(), false)
+
+        when (action.deviceType) {
+            DeviceType.BUTTON -> handleButtonAction(action)
+            DeviceType.CARD -> handleCardReaderAction(action)
+            else -> return
+        }
+    }
+
+    @Throws(IllegalArgumentException::class)
+    private fun handleButtonAction(action: Action) {
+        if (action.deviceType != DeviceType.BUTTON)
+            throw IllegalArgumentException("Action is not a button action")
+
+        val isPressed = action.value!! == 1
+        when (action.deviceId) {
+            0 -> changeButtonState(button0, isPressed)
+            1 -> changeButtonState(button1, isPressed)
+            2 -> changeButtonState(button2, isPressed)
+            3 -> changeButtonState(button3, isPressed)
+            4 -> changeButtonState(button4, isPressed)
+            5 -> changeButtonState(button5, isPressed)
+        }
+    }
+
+    private fun handleCardReaderAction(action: Action) {
+        if (action.deviceType != DeviceType.CARD)
+            throw IllegalArgumentException("Action is not a card reader action")
+
+        val id = action.value!!
+
+        userIdLabel.text = if (id < 0)
+            "(card not present)"
+        else
+            id.toString()
     }
 
     override fun onStateReceived(state: StateMessage) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        addToLogTextView(state.toYaml(), false)
     }
 
     override fun onAvailablePortsChange() {
@@ -175,6 +231,13 @@ class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvaila
         addToLogTextView(data, true)
     }
 
+    private fun changeButtonState(button: Styleable, pressed: Boolean) {
+        if (pressed)
+            button.styleClass.add(buttonPressedStyle)
+        else
+            button.styleClass.remove(buttonPressedStyle)
+    }
+
     /**
      * Adds the given [text] to the serial communication log view,
      * if [sent] is set to true, the text will be prepended with >
@@ -186,5 +249,9 @@ class MaintenanceController : Initializable, OnMessageReceivedListener, OnAvaila
         val indentedText = text.prependIndent("\t")
 
         logText.appendText(directionChar + indentedText + "\n\n")
+    }
+
+    override fun onDataReceived(data: String) {
+        messageParser.parse(data)
     }
 }

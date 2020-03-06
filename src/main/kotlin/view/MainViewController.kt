d43a09951236e5a73d0930de4ef7b28f0ab141a7
AdamTMalek <malekadas@gmail.com>
2020-03-06 20:14:35 +0000

    Add serial port control to the main view and 2 buttons (game + debug)

--- src/main/kotlin/view/MainViewController.kt
+++ src/main/kotlin/view/MainViewController.kt
@@ -3,18 +3,24 @@ package view
 import app.Language
 import app.Player
 import app.PlayersDatabase
+import app.serialcom.OnAvailablePortsChangeListener
 import app.serialcom.Serial
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.ChoiceBox
 import javafx.scene.input.KeyCode
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
-class MainViewController : Initializable {
+class MainViewController : Initializable, OnAvailablePortsChangeListener {
     @FXML
     private var rootPane = Pane()
+
+    @FXML
+    private var portChoice = ChoiceBox<String>()
 
     private lateinit var serial: Serial
 
@@ -23,6 +29,12 @@ class MainViewController : Initializable {
 
     fun setSerial(serial: Serial) {
         this.serial = serial
+        serial.addPortListener(this)
+
+        Platform.runLater {
+            setupPortChoice()
+            addPortChoiceChangeListener()
+        }
     }
 
     /**
@@ -74,6 +86,43 @@ class MainViewController : Initializable {
         EndScreenController.loadWithAnimation(rootPane, player, 1, isNewTopScore)
     }
 
+    override fun onAvailablePortsChange() {
+        Platform.runLater {
+            portChoice.items.clear()
+            portChoice.items.addAll(getAllAvailablePortsNames())
+        }
+    }
+
+    /**
+     * Picks the current com port in the choice box if [Serial.getActivePort] returns non-null value
+     */
+    private fun setupPortChoice() {
+        val serialNames = getAllAvailablePortsNames()
+
+        portChoice.items.addAll(serialNames)
+
+        val current = serial.getActivePort()
+            ?: return
+
+        val currentPortIndex = portChoice.items.indexOf(current.descriptivePortName)
+        portChoice.selectionModel.select(currentPortIndex)
+    }
+
+    private fun addPortChoiceChangeListener() {
+        portChoice.selectionModel.selectedItemProperty().addListener { _, _, newPort ->
+            if (newPort == null)
+                return@addListener
+
+            val port = serial.getAllAvailablePorts().find { it.descriptivePortName == newPort }!!
+            serial.connectTo(port)
+        }
+    }
+
+    /**
+     * Returns a list of descriptive port names of the available ports
+     */
+    private fun getAllAvailablePortsNames() = serial.getAllAvailablePorts().map { it.descriptivePortName }
+
     fun openMaintenanceWindow() {
         MaintenanceController.showAndWait(serial)
     }
@@ -84,6 +133,7 @@ class MainViewController : Initializable {
 
     fun close() {
         val stage = rootPane.scene.window as Stage
+        serial.removePortListener(this)
         stage.close()
     }
 }
